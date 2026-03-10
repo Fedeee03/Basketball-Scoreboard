5e59972639eb4a7ce86b80753894a9a03afde3b5
Federico Vega <fedeveg123@gmail.com>
2026-03-10 16:40:24 -0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,22 @@
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreEl = document.getElementById("home-score");
+let guestScoreEl = document.getElementById("guest-score");
+
+function addPoints(team,points) {
+    if (team === "home") {
+        homeScore += points;
+        homeScoreEl.textContent = homeScore;
+    } else if (team === "guest") {
+        guestScore += points;
+        guestScoreEl.textContent = guestScore;
+    }
+}
+
+function resetScores() {
+    homeScore = 0;
+    guestScore = 0;
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+}
